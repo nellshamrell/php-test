5ab52b4116e6e41581a40c54d98ae8c0bea24817
Nell Shamrell <nellshamrell@gmail.com>
2018-04-10 15:49:48 -0700

    wip
    Signed-off-by: Nell Shamrell <nellshamrell@gmail.com>

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -3,9 +3,11 @@ pkg_origin=core
 pkg_version="0.1.0"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=("Apache-2.0")
-pkg_source="https://github.com/nellshamrell/sample-php-habitat/archive/0.1.0.tar.gz"
+pkg_source="https://github.com/nellshamrell/php-test/archive/0.1.0.tar.gz"
 pkg_deps=(core/make core/php core/nginx)
-pkg_shasum=2b1e1474d1a8bff181771a37dd70d988c34149144532bb4f8030e8c6d9d2d0bc
+pkg_shasum=bde86f0d969211d3ad9162024785a414805a2247296821c9a1c9a2d37c03c375
+pkg_svc_user="root"
+pkg_svc_group=$pkg_svc_user
 
 do_build() {
     return 0
